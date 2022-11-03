05c96c4adb101cdc998a7220b2de55cf358fe737
Arnav K <arnavkaushal09@gmail.com>
2022-11-03 10:18:22 +0000

    Rmoved Bin

--- main.js
+++ main.js
@@ -21,12 +21,12 @@ app.use(express.static('./public'));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -34,6 +34,16 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+/***Starting Sever***/
+var PORT = 3000;
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log("🟢 Server is Successfully Running, and App is listening on port " + PORT)
+  else {
+    console.log("🔴 Error occurred, server can't start\n\t", error);
+  }
 });
 
 // exporting app 
